c4956bb2e408cca91a72d76c08b7085d3d3973ab
Y-warrior <110579596+Y-warrior@users.noreply.github.com>
2026-01-29 23:24:57 -0300

    Add urgency feature to AbstractDocumentoBase
    Added 'urgente' field with getter and setter methods to manage document urgency.

--- src/br/ifba/edu/inf011/model/documentos/AbstractDocumentoBase.java
+++ src/br/ifba/edu/inf011/model/documentos/AbstractDocumentoBase.java
@@ -1,77 +1,80 @@
 package br.ifba.edu.inf011.model.documentos;
-
 import java.time.LocalDateTime;
 import java.util.Set;
-
 import br.ifba.edu.inf011.model.Assinatura;
 import br.ifba.edu.inf011.model.FWDocumentException;
 import br.ifba.edu.inf011.model.operador.Operador;
-
-public abstract class AbstractDocumentoBase implements Documento{
-
+public abstract class AbstractDocumentoBase implements Documento {
     protected String numero;
     protected String conteudo;
-    protected Operador proprietario; 
+    protected Operador proprietario;
     protected LocalDateTime dataCriacao;
     protected Privacidade privacidade;
-    
-    public AbstractDocumentoBase(String conteudo, Operador proprietario, 
-			 LocalDateTime dataCriacao, Privacidade privacidade, Set<Assinatura> assinaturas) {
-		this.conteudo = conteudo;
-		this.proprietario = proprietario;
-		this.dataCriacao = dataCriacao;
-		this.privacidade = privacidade;
-    }    
-    
+    protected boolean urgente;
+
+    public AbstractDocumentoBase(String conteudo, Operador proprietario,
+                                 LocalDateTime dataCriacao, Privacidade privacidade, Set<Assinatura> assinaturas) {
+        this.conteudo = conteudo;
+        this.proprietario = proprietario;
+        this.dataCriacao = dataCriacao;
+        this.privacidade = privacidade;
+        this.urgente = false; 
+    }
+
     public AbstractDocumentoBase(Operador proprietario, Privacidade privacidade) {
-    	this(null, proprietario, LocalDateTime.now(), privacidade, null);
-    }    
-    
+        this(null, proprietario, LocalDateTime.now(), privacidade, null);
+    }
+
     public AbstractDocumentoBase() {
-    	this(null, null, LocalDateTime.now(), null, null);
-    }       
+        this(null, null, LocalDateTime.now(), null, null);
+    }
 
     @Override
-	public void inicializar(Operador proprietario, Privacidade privacidade) {
-		this.proprietario = proprietario;
-		this.privacidade = privacidade;
-		
-	}
+    public void inicializar(Operador proprietario, Privacidade privacidade) {
+        this.proprietario = proprietario;
+        this.privacidade = privacidade;
+    }
 
-	@Override
-	public String getNumero() {
-		return this.numero;
-	}
+    @Override
+    public String getNumero() {
+        return this.numero;
+    }
+    @Override
+    public String getConteudo() throws FWDocumentException {
+        return this.conteudo;
+    }
 
-	@Override
-	public String getConteudo() throws FWDocumentException{
-		return this.conteudo;
-	}
+    @Override
+    public Operador getProprietario() {
+        return this.proprietario;
+    }
 
-	@Override
-	public Operador getProprietario() {
-		return this.proprietario;
-	}
-	
-	@Override
-	public Privacidade getPrivacidade() {
-		return this.privacidade;
-	}	
-	
-	@Override
-	public void setConteudo(String conteudo) {
-		this.conteudo = conteudo;
-	}	
-	
-	@Override
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-    
-    // Exemplo simples de representação do documento
+    @Override
+    public Privacidade getPrivacidade() {
+        return this.privacidade;
+    }
+
+    @Override
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    @Override
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    @Override
+    public boolean isUrgente() {
+        return this.urgente;
+    }
+    @Override
+    public void setUrgente(boolean urgente) {
+        this.urgente = urgente;
+    }
     @Override
     public String toString() {
-        return  this.getClass().getSimpleName() + "{" +
+        String tag = this.urgente ? "[URGENTE] " : "";
+        return tag + this.getClass().getSimpleName() + "{" +
                 "numero='" + numero + '\'' +
                 ", proprietario='" + proprietario + '\'' +
                 ", dataCriacao=" + dataCriacao +
