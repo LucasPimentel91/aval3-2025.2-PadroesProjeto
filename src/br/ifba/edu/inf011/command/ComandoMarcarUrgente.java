6e643b34ea0694dceb12026b613be97525b352cd
Y-warrior <110579596+Y-warrior@users.noreply.github.com>
2026-01-29 23:31:15 -0300

    Refactor execute method to handle null target

--- src/br/ifba/edu/inf011/command/ComandoMarcarUrgente.java
+++ src/br/ifba/edu/inf011/command/ComandoMarcarUrgente.java
@@ -2,18 +2,17 @@ package br.ifba.edu.inf011.command;
 import br.ifba.edu.inf011.model.GerenciadorDocumentoModel;
 import br.ifba.edu.inf011.model.documentos.Documento;
 public class ComandoMarcarUrgente extends BaseDocumentoCommand {
-
     public ComandoMarcarUrgente(GerenciadorDocumentoModel model, Documento target) {
         super(model, target);
     }
-
     @Override
     public void execute() throws Exception {
-        this.before = model.createMemento(target);
-        Documento urgent = model.applyUrgent(target);
-        this.after = model.createMemento(urgent);
+        if (this.target == null) return;
+        this.before = model.createMemento(this.target);
+        Documento urgente = model.applyUrgent(this.target);
+        this.target = urgente;
+        this.after = model.createMemento(this.target);
     }
-
     @Override
     public String getDescription() {
         return "Tornar urgente";
