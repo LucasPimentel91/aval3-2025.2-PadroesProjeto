1d8f9bef49b2f880e7077338c528603c329ab7ac
Y-warrior <110579596+Y-warrior@users.noreply.github.com>
2026-01-30 00:05:57 -0300

    Refactor SeloUrgenciaDecorator for clarity

--- src/br/ifba/edu/inf011/decorator/SeloUrgenciaDecorator.java
+++ src/br/ifba/edu/inf011/decorator/SeloUrgenciaDecorator.java
@@ -2,7 +2,6 @@ package br.ifba.edu.inf011.decorator;
 import br.ifba.edu.inf011.model.FWDocumentException;
 import br.ifba.edu.inf011.model.documentos.Documento;
 public class SeloUrgenciaDecorator extends DocumentoDecorator {
-
     public SeloUrgenciaDecorator(Documento d) {
         super(d);
         if (d != null) {
@@ -11,15 +10,15 @@ public class SeloUrgenciaDecorator extends DocumentoDecorator {
     }
     @Override
     public String getConteudo() throws FWDocumentException {
-        String conteudo = super.getConteudo();
-        if (conteudo == null) {
-            conteudo = "";
+        String base = super.getConteudo();
+        if (base == null) {
+            base = "";
         }
 
-        if (conteudo.startsWith("[URGENTE]")) {
-            return conteudo;
+        if (base.startsWith("[URGENTE]")) {
+            return base;
         }
 
-        return "[URGENTE]\n" + conteudo;
+        return "[URGENTE]\n" + base;
     }
 }
