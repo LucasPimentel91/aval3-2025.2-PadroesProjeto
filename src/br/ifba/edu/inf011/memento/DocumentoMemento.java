d7067db74e701a852aa1466b323d2b885bd99ac6
Y-warrior <110579596+Y-warrior@users.noreply.github.com>
2026-01-29 23:33:07 -0300

    Add urgente field to DocumentoMemento class

--- src/br/ifba/edu/inf011/memento/DocumentoMemento.java
+++ src/br/ifba/edu/inf011/memento/DocumentoMemento.java
@@ -1,33 +1,26 @@
 package br.ifba.edu.inf011.memento;
-
 import br.ifba.edu.inf011.model.documentos.Documento;
-
-/**
- * Memento (Questão II)
- *
- * Captura o estado necessário para restaurar um documento no repositório.
- */
 public final class DocumentoMemento {
-
     private final int index;
     private final Documento documentoRef;
     private final String conteudo;
-
-    public DocumentoMemento(int index, Documento documentoRef, String conteudo) {
+    private final Boolean urgente;
+    public DocumentoMemento(int index, Documento documentoRef, String conteudo, Boolean urgente) {
         this.index = index;
         this.documentoRef = documentoRef;
         this.conteudo = conteudo;
+        this.urgente = urgente;
     }
-
     public int getIndex() {
         return index;
     }
-
     public Documento getDocumentoRef() {
         return documentoRef;
     }
-
     public String getConteudo() {
         return conteudo;
+    }
+    public Boolean getUrgente() {
+        return urgente;
     }
 }
