ec4151dd54d47911d81250ac381d826336dbc194
Y-warrior <110579596+Y-warrior@users.noreply.github.com>
2026-01-29 23:22:39 -0300

    Add urgency methods to Documento interface

--- src/br/ifba/edu/inf011/model/documentos/Documento.java
+++ src/br/ifba/edu/inf011/model/documentos/Documento.java
@@ -1,15 +1,15 @@
 package br.ifba.edu.inf011.model.documentos;
-
 import br.ifba.edu.inf011.model.FWDocumentException;
 import br.ifba.edu.inf011.model.operador.Operador;
 import br.ifba.edu.inf011.prototype.Prototipavel;
-
-public interface Documento extends Prototipavel{
-	public void	inicializar(Operador proprietario, Privacidade privacidade) throws FWDocumentException;
-	public void setConteudo(String conteudo);
-	public void setNumero(String numero);
-    public String getNumero();
-    public String getConteudo() throws FWDocumentException;
-    public Operador getProprietario();
-    public Privacidade getPrivacidade();
+public interface Documento extends Prototipavel {
+    void inicializar(Operador proprietario, Privacidade privacidade) throws FWDocumentException;
+    void setConteudo(String conteudo);
+    void setNumero(String numero);
+    String getNumero();
+    String getConteudo() throws FWDocumentException;
+    Operador getProprietario();
+    Privacidade getPrivacidade();
+    boolean isUrgente();
+    void setUrgente(boolean urgente);
 }
