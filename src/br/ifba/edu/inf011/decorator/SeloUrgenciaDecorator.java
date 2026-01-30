badc25ceb070e08b076d487e7a8e62dbebc936d7
Y-warrior <110579596+Y-warrior@users.noreply.github.com>
2026-01-29 23:59:40 -0300

    Enhance SeloUrgenciaDecorator with null check
    Added null check for document content and set urgent flag.

--- src/br/ifba/edu/inf011/decorator/SeloUrgenciaDecorator.java
+++ src/br/ifba/edu/inf011/decorator/SeloUrgenciaDecorator.java
@@ -1,17 +1,25 @@
 package br.ifba.edu.inf011.decorator;
-
 import br.ifba.edu.inf011.model.FWDocumentException;
 import br.ifba.edu.inf011.model.documentos.Documento;
-
 public class SeloUrgenciaDecorator extends DocumentoDecorator {
-	
-    public SeloUrgenciaDecorator(Documento d) {
-    	super(d); 
-    }
-    
-	@Override
-	public String getConteudo() throws FWDocumentException{
-		return "[URGENTE]\n" + super.getConteudo();
-	}    
 
+    public SeloUrgenciaDecorator(Documento d) {
+        super(d);
+        if (d != null) {
+            d.setUrgente(true);
+        }
+    }
+    @Override
+    public String getConteudo() throws FWDocumentException {
+        String conteudo = super.getConteudo();
+        if (conteudo == null) {
+            conteudo = "";
+        }
+
+        if (conteudo.startsWith("[URGENTE]")) {
+            return conteudo;
+        }
+
+        return "[URGENTE]\n" + conteudo;
+    }
 }
